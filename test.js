3a923a69c285a6470368f7aac4821c756c2ddf08
Titus Wormer <tituswormer@gmail.com>
2022-11-16 16:08:15 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,22 +1,18 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {webNamespaces} from './index.js'
 
 const own = {}.hasOwnProperty
 
-test('webNamespaces', function (t) {
-  t.equal(typeof webNamespaces, 'object', 'should be an `object`')
+test('webNamespaces', function () {
+  assert.equal(typeof webNamespaces, 'object', 'should be an `object`')
 
-  t.doesNotThrow(function () {
-    /** @type {string} */
-    let key
+  /** @type {string} */
+  let key
 
-    for (key in webNamespaces) {
-      if (own.call(webNamespaces, key)) {
-        assert.equal(typeof webNamespaces[key], 'string', key)
-      }
+  for (key in webNamespaces) {
+    if (own.call(webNamespaces, key)) {
+      assert.equal(typeof webNamespaces[key], 'string', key)
     }
-  }, 'should be all `string`s')
-
-  t.end()
+  }
 })
